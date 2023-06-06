4590d0d114d0832086fcb676122a25d39561f099
Den <delton.denice@gmail.com>
2023-06-06 21:32:42 +0200

    added Dtos and validation dependancies

---
+++ src/main/java/fr/esgi/properties/dto/request/RentalPropertyRequestDto.java
@@ -0,0 +1,25 @@
+package fr.esgi.properties.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+@Builder
+public record RentalPropertyRequestDto(
+        @NotNull String description,
+        @NotNull String town,
+        @NotNull String address,
+        @NotNull String propertyType,
+        @NotNull double rentAmount,
+        @NotNull double securityDepositAmount,
+        @NotNull double area,
+        @NotNull int bedroomsCount,
+        int floorNumber,
+        int numberOfFloors,
+        @NotNull int constructionYear,
+        @NotNull String energyClassification,
+        @NotNull boolean hasElevator,
+        @NotNull boolean hasIntercom,
+        @NotNull boolean hasBalcony,
+        @NotNull boolean hasParkingSpace) {
+
+}
