f17967861ef8db082d901c570d9fd4fe422b106e
Den <delton.denice@gmail.com>
2023-06-06 17:00:00 +0200

    Initial

---
+++ src/test/java/fr/esgi/properties/ApplicationTest.java
@@ -0,0 +1,15 @@
+package fr.esgi.properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
+
