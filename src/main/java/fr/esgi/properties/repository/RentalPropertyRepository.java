475085a41454cd26f551faf72230f491ca7fd6a0
Den <delton.denice@gmail.com>
2023-06-06 18:27:57 +0200

    added RentalPropertyRepository

---
+++ src/main/java/fr/esgi/properties/repository/RentalPropertyRepository.java
@@ -0,0 +1,12 @@
+package fr.esgi.properties.repository;
+
+import fr.esgi.properties.domain.RentalPropertyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RentalPropertyRepository extends JpaRepository<RentalPropertyEntity, UUID> {
+
+}
