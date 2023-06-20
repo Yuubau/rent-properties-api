df6046dbdad5a9f089efaceced14ed9575147845
Den <delton.denice@gmail.com>
2023-06-20 15:47:53 +0200

    fix tests

--- src/test/java/fr/esgi/properties/mapper/RentalPropertyDtoMapperTest.java
+++ src/test/java/fr/esgi/properties/mapper/RentalPropertyDtoMapperTest.java
@@ -1,6 +1,8 @@
 package fr.esgi.properties.mapper;
 
+import fr.esgi.properties.domain.PropertyTypeEntity;
 import fr.esgi.properties.domain.RentalPropertyEntity;
+import fr.esgi.properties.dto.request.RentalPropertyRequestDto;
 import fr.esgi.properties.dto.response.RentalPropertyResponseDto;
 import org.junit.jupiter.api.Test;
 
@@ -50,26 +52,28 @@ class RentalPropertyDtoMapperTest {
 
     @Test
     void shouldMapToEntity() {
-        List<RentalPropertyEntity> rentalPropertyEntities = rentalPropertyEntities();
+        RentalPropertyRequestDto dto = new RentalPropertyRequestDto(
+                "Test",
+                "Paris",
+                "242 rue du faubourg saint antoine",
+                null,
+                1000.00d,
+                2000.00d,
+                10.00d,
+                3,
+                2,
+                6,
+                (char)2003,
+                null,
+                true,
+                true,
+                true,
+                true
+        );
 
         RentalPropertyDtoMapper rentalPropertyDtoMapper = new RentalPropertyDtoMapper();
-
-        List<RentalPropertyResponseDto> rentalPropertyResponseList = rentalPropertyDtoMapper.mapToDtoList(rentalPropertyEntities);
-
-        assertThat(rentalPropertyResponseList).isNotNull()
-                .hasSize(1)
-                .extracting("description", "address", "town", "propertyType", "rentAmount", "securityDepositAmount", "area")
-                .containsExactlyInAnyOrder(
-                        tuple("Appartement spacieux avec vue sur l'ESGI",
-                                "77 Rue des roses",
-                                "Paris",
-                                "Appartement",
-                                750.90,
-                                1200.90,
-                                37.48)
-                );
-
-        rentalPropertyDtoMapper.mapToEntity(rentalPropertyResponseList.get(0))
+        RentalPropertyEntity entity = rentalPropertyDtoMapper.mapToEntity(dto);
+        assertThat(entity).hasFieldOrPropertyWithValue("securityDepositAmount", 2000.00d);
     }
 
 }
